726ccc2318f6c234a97d7f6f774ec06181a65b49
Cesar A. Rivas <crivasg@gmail.com>
2022-09-06 13:14:53 -0500

    Create __init__.py

---
+++ ine/__init__.py
@@ -0,0 +1,6 @@
+'''__init__.py'''
+
+__author__  = 'Cesar A. Rivas'
+__version__ = '0.1.0'
+__desc__ = 'process the INE results from a zip file'
+__url__ = 'https://computosrm2022.ine.mx/assets/JSON/REVOCACION_MANDATO/NACIONAL/Revocacion_Mandato_NACIONAL.json'
