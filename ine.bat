d1c3c53aa2e6abfbb36712c76df79a96ef3caf59
Cesar A. Rivas <crivasg@gmail.com>
2022-09-10 13:26:08 -0500

    Update ine.bat

--- ine.bat
+++ ine.bat
@@ -12,6 +12,20 @@ echo #                                                            #
 echo #............................................................#
 echo.
 
+if not exist "venv\Scripts\activate.bat" (
+	echo.
+	echo The virtal enviroment does not exist. 
+	echo Will try to make the enviroment.
+	echo.
+	
+	python -m venv venv
+	call venv\Scripts\activate.bat
+	python -m pip install -r requirements.txt
+	:: python -m pip install --upgrade --force-reinstall -r requirements.txt
+	call venv\Scripts\deactivate.bat
+
+)
+
 call venv\Scripts\activate.bat
 python -m ine
 call venv\Scripts\deactivate.bat
