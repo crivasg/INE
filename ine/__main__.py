5794d08918ab4cc9cbc5ff99edae7c348397b886
Cesar A. Rivas <crivasg@gmail.com>
2022-09-10 11:20:50 -0500

    Update __main__.py

--- ine/__main__.py
+++ ine/__main__.py
@@ -1,4 +1,16 @@
 '''__main__.py'''
 
+import datetime
+import pathlib
+import os
+import re
+import sys
+import zipfile
+
+import ine
+
+def main():
+    pass
+
 if __name__ == '__main__':
     main()
