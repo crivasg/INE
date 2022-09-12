3bcaa244d2d0f9b4ec7aa6808d22954b940e6bf7
Cesar A. Rivas <crivasg@gmail.com>
2022-09-12 15:56:49 -0500

    Update __main__.py
    added import statements into the main file.

--- ine/__main__.py
+++ ine/__main__.py
@@ -7,7 +7,13 @@ import re
 import sys
 import zipfile
 
+from urllib.parse import urljoin, urlsplit, urlunsplit, urlparse
+
 import ine
+
+from ine.cli import read_user_cli_args
+from ine.data import read_and_store_data, print_summary_data, print_state_summary
+from ine.web_requests import download_file, request_json_url
 
 def main():
     pass
