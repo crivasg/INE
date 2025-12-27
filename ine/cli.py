2a090e51292ed71855ea7de1e95028a78b04bec8
Cesar A. Rivas <crivasg@gmail.com>
2025-12-27 10:42:35 -0600

    Update cli.py

--- ine/cli.py
+++ ine/cli.py
@@ -9,17 +9,17 @@ def read_user_cli_args():
     parser = argparse.ArgumentParser(prog='ine',
                                      description='process the INE results from a zip file')
 
-    '''controls the output to the screen.'''
-    parser.add_argument('-v','--verbose',
-                        required=False,
-                        action='store_true',
-                        default=False,
-                        help='Increase output verbosity')
-    
+    '''controls the output to the screen.'''    
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-v", "--verbose", 
-                       action="store_true")
+                       required=False,
+                       action="store_true",
+                       default=False,
+                       help='Increase output verbosity')
     group.add_argument("-q", "--quiet", 
-                       action="store_true")
+                       required=False,
+                       action="store_true"
+                      default=True,help='Make the program quiet')
 
     return parser.parse_args()
+
